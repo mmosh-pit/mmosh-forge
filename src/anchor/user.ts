643b948691ad65799de1a388404055cd3855710b
balajimara <balaji.jeyakumar@mara.com>
2024-03-06 00:54:00 +0530

    updating transaction timeout issue

--- src/anchor/user.ts
+++ src/anchor/user.ts
@@ -399,7 +399,7 @@ export class Connectivity {
       // const txLen = signedTx.serialize().length;
       // log({ txLen, luts: lutsInfo.length });
 
-      const blockhash = (await this.connection.getLatestBlockhash("confirmed")).blockhash;
+      const blockhash = (await this.connection.getLatestBlockhash("finalized")).blockhash;
       console.log("recentBlockhash: confirmed ", blockhash);
       const message = new web3.TransactionMessage({
         payerKey: this.provider.publicKey,
